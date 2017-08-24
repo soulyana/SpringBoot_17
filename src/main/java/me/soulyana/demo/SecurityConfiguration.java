fdea8e45b08d055061469bab5d85e8d8cc37dc08
soulyana <soulakew@umd.edu>
2017-08-24 14:03:15 -0400

    second commit - edit security configuration

--- src/main/java/me/soulyana/demo/SecurityConfiguration.java
+++ src/main/java/me/soulyana/demo/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package me.soulyana.demo;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,4 +21,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
                 .and()
                 .httpBasic();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    }
 }
+
